0e8e23cde7f71dce2fd6723c0e6fc015158666cd
jotasie7e <juanseabril@gmail.com>
2019-06-01 12:23:52 -0500

    Tabs Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,58 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:fllutter_app/pages/home.dart';
+import 'package:fllutter_app/pages/contact.dart';
+import 'package:fllutter_app/pages/video.dart';
 
-void main(){
+
+void main() {
   runApp(new MaterialApp(
-    home: MyStepper(),
+    home: MyTabs(),
   ));
 }
 
-class MyStepper extends StatefulWidget{
+
+class MyTabs extends StatefulWidget {
   @override
-  _MyStepperState createState() => new _MyStepperState();
+  _MyTabsState createState() => new _MyTabsState();
 }
 
-class _MyStepperState extends State<MyStepper>{
 
-  int _currentStep = 0;
-  List<Step> mySteps = [
-    new Step(title: new Text("Paso 1"), content: new Text("Aprendiendo")),
-    new Step(title: new Text("Paso 2"), content: new Text("Flutter")),
-    new Step(title: new Text("Paso 3"), content: new Text(";)")),
-  ];
-
+class _MyTabsState extends State<MyTabs>  with SingleTickerProviderStateMixin {
+  TabController controller;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    controller = new TabController(length: 3, vsync: this);
+  }
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("My Stepper"),
-      ),
-      body: new Container(
-        child: new Stepper(
-          currentStep: this._currentStep,
-          steps: mySteps,
-          //type: StepperType.horizontal,
-          onStepContinue: (){
-            setState(() {
-              if(_currentStep < mySteps.length - 1) {
-                _currentStep = _currentStep + 1;
-              }
-              else{
-                _currentStep = 0;
-              }
-            });
-          },
-          onStepCancel: (){
-            setState(() {
-              _currentStep = 0;
-            });
-          },
-          onStepTapped: (step){
-            setState(() {
-              _currentStep = step;
-            });
-          },
+        appBar: new AppBar(
+          title: new Text("Video Tabs"),
+          backgroundColor: Colors.redAccent,
+          bottom: new TabBar(
+            tabs: <Widget>[
+              new Tab(
+                icon: new Icon(Icons.home),
+              ),
+              new Tab(
+                icon: new Icon(Icons.ondemand_video),
+              ),
+              new Tab(
+                icon: new Icon(Icons.contacts),
+              )
+            ],
+            controller: controller,
+          ),
+        ),
+        body: new TabBarView(
+            children: <Widget>[
+              new Home(), new Video(), new Contact()
+            ],
+            controller: controller
         )
-      ),
     );
   }
 }
